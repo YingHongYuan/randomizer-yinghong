effaa3f57918aae85e8272cd8c48e86424c1a0de
YingHongYuan <yinghong.yuan@sjsu.edu>
2020-03-09 17:38:14 -0700

    1

--- sketch.js
+++ sketch.js
@@ -21,7 +21,9 @@ let randomIndex;
 function setup() {
   createCanvas(600, 600);
   background(100);
+  textSize(32);
 
+  text("click to randomize",250,500);
 }
 
 function draw() {
@@ -29,8 +31,15 @@ function draw() {
 }
 
 function mousePressed(){
-  background(random(255));
+
+  if(flowers[0]){
+  background(random(200,255));
   randomIndex = int(random(flowers.length));
-  text(flowers[randomIndex].name,30 0,500);
+  text(flowers[randomIndex].name,250,500);
   flowers.splice(randomIndex,1);
+}
+else {
+  background(random(200,255));
+  text("nothing left!",250,500);
+}
 }
