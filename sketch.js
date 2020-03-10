ab14014de8e8495dc3384289203709398eaf2d20
YingHongYuan <yinghong.yuan@sjsu.edu>
2020-03-09 17:11:04 -0700

    1

--- sketch.js
+++ sketch.js
@@ -1,13 +1,36 @@
 // the flower I like
-let flowers = ["sunflower","rose","lily","tulip","babysbreath"];
+let flowers = [{
+  name:"sunflower",
+  color: "yellow"
+},{
+  name:"rose",
+  color: "red"
+},{
+  name:"lily",
+  color: "white"
+},{
+  name:"tulip",
+  color: "orange"
+},{
+  name:"babysbreath",
+  color: "purple"
+}];
+
+let randomIndex;
 
 function setup() {
   createCanvas(600, 600);
-  background(200);
+  background(100);
 
-  console.log(flower[2]);
 }
 
 function draw() {
 
+}
+
+function mousePressed(){
+  background(random(255));
+  randomIndex = int(random(flowers.length));
+  text(flowers[randomIndex].name,30 0,500);
+  flowers.splice(randomIndex,1);
 }
